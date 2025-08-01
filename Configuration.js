d710b24f65b573a389c84c5033da51f6157fbbab
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-08-01 16:44:35 -0300

    Update Configuration.js
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

--- Configuration.js
+++ Configuration.js
@@ -24,29 +24,16 @@ function getEndpoints(deviceAddress, endpoints)
 
   endpoints.addEndpoint("1", "Fase A Sensor de Voltaje", endpointType.voltageSensor);
   endpoints.addEndpoint("2", "Fase A Sensor de Corriente", endpointType.currentSensor);
-  endpoints.addEndpoint("3", "Fase A Factor de Potencia", endpointType.cosPhiSensor);
-  var ae = endpoints.addEndpoint("4", "Fase A Energía Activa", endpointType.genericSensor);
+  var ae = endpoints.addEndpoint("4", "Energía Activa", endpointType.genericSensor);
   ae.variableTypeId = 1440;
-  var re = endpoints.addEndpoint("5", "Fase A Energía Reactiva", endpointType.genericSensor);
-  re.variableTypeId = 1441;
   endpoints.addEndpoint("6", "Fase A Potencia Aparente", endpointType.apparentPowerSensor);
   endpoints.addEndpoint("7", "Fase A Potencia Activa", endpointType.activePowerSensor);
   endpoints.addEndpoint("8", "Fase B Sensor de Voltaje", endpointType.voltageSensor);
   endpoints.addEndpoint("9", "Fase B Sensor de Corriente", endpointType.currentSensor);
-  endpoints.addEndpoint("10", "Fase B Factor de Potencia", endpointType.cosPhiSensor);
-  var ae = endpoints.addEndpoint("11", "Fase B Energía Activa", endpointType.genericSensor);
-  ae.variableTypeId = 1440;
-  var re = endpoints.addEndpoint("12", "Fase B Energía Reactiva", endpointType.genericSensor);
-  re.variableTypeId = 1441;
   endpoints.addEndpoint("13", "Fase B Potencia Aparente", endpointType.apparentPowerSensor);
   endpoints.addEndpoint("14", "Fase B Potencia Activa", endpointType.activePowerSensor);
   endpoints.addEndpoint("15", "Fase C Sensor de Voltaje", endpointType.voltageSensor);
   endpoints.addEndpoint("16", "Fase C Sensor de Corriente", endpointType.currentSensor);
-  endpoints.addEndpoint("17", "Fase C Factor de Potencia", endpointType.cosPhiSensor);
-  var ae = endpoints.addEndpoint("18", "Fase C Energía Activa", endpointType.genericSensor);
-  ae.variableTypeId = 1440;
-  var re = endpoints.addEndpoint("19", "Fase C Energía Reactiva", endpointType.genericSensor);
-  re.variableTypeId = 1441;
   endpoints.addEndpoint("20", "Fase C Potencia Aparente", endpointType.apparentPowerSensor);
   endpoints.addEndpoint("21", "Fase C Potencia Activa", endpointType.activePowerSensor);
   endpoints.addEndpoint("22", "Potencia Activa Total", endpointType.activePowerSensor);
@@ -98,6 +85,6 @@ function updateEndpointUIRules(endpoint, rules)
   // - The endpoint subtype can be changed, but only for the second 
   //   endpoint.
   
-  // rules.canDelete = false;
+  rules.canDelete = true;
   // rules.canEditSubType = (endpoint.address == "2");
 }
